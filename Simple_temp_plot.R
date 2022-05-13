2705b161e830778238fceb063f4955577fc0fe33
Dr-Ama-Llama <105367365+Dr-Ama-Llama@users.noreply.github.com>
2022-05-13 16:48:31 +0200

    Update Simple_temp_plot.R

--- Simple_temp_plot.R
+++ Simple_temp_plot.R
@@ -4,9 +4,6 @@ library(readr)
 weather_data="https://raw.githubusercontent.com/Dr-Ama-Llama/CIMOverflow_git_intro/main/Islamorada_0517.csv"
 Isla_0517 <- read.csv(url(weather_data), sep = ";")
 
-ggplot(Isla_0517, aes(Day, High))+
-	geom_point(aes(color = High)) +
-	theme(legend.position = "bottom") + theme(axis.text.x = element_text(angle = 45))
 
 Isla_0517HighC <- (((Isla_0517$High)-32)*(5/9))
 Isla_0517$HighC <- Isla_0517HighC
