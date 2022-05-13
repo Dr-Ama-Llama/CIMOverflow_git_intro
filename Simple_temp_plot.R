92c07509602577cf4039e382b424f6cc15cd3267
Dr-Ama-Llama <105367365+Dr-Ama-Llama@users.noreply.github.com>
2022-05-13 16:36:00 +0200

    Update Simple_temp_plot.R
    Now its in the correct temperature scale

--- Simple_temp_plot.R
+++ Simple_temp_plot.R
@@ -7,3 +7,10 @@ Isla_0517 <- read.csv(url(weather_data), sep = ";")
 ggplot(Isla_0517, aes(Day, High))+
 	geom_point(aes(color = High)) +
 	theme(legend.position = "bottom") + theme(axis.text.x = element_text(angle = 45))
+
+Isla_0517HighC <- (((Isla_0517$High)-32)*(5/9))
+Isla_0517$HighC <- Isla_0517HighC
+
+ggplot(Isla_0517, aes(Day, HighC))+
+	geom_point(aes(color = HighC)) +
+	theme(legend.position = "bottom") + theme(axis.text.x = element_text(angle = 45))
